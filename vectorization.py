300f7f2a0d6a31e774701e1cb60185c2b847bb58
Sanskruti Raut <sanskrutiraut14@gmail.com>
2020-06-01 13:50:54 +0530

    First commit

---
+++ vectorization.py
@@ -0,0 +1,25 @@
+import numpy as np
+a= np.array([1,2,3,4])
+print (a)
+
+import time
+a=np.random.rand(1000000)
+b=np.random.rand(1000000)
+tic=time.time()
+c=np.dot(a,b)
+toc=time.time()
+
+print (c)
+print("Vectorized version :" + str((toc-tic)) + "ms")
+
+c=0
+tic=time.time()
+for i in range (1000000):
+      c+= a[i]*b[i]
+toc=time.time()
+print (c)
+print ("for loop :" +str((toc-tic))+ "ms")
+      
+      
+      
+      
