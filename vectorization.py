be882633547e1867eda8abeef2e06187ce3f40ef
Aniket Raj <aniketronaldo10@gmail.com>
2020-10-01 00:40:00 +0530

    Cleaned code and well documented it

--- vectorization.py
+++ vectorization.py
@@ -1,25 +1,30 @@
+# Import Libraries
 import numpy as np
-a= np.array([1,2,3,4])
-print (a)
-
 import time
-a=np.random.rand(1000000)
-b=np.random.rand(1000000)
-tic=time.time()
-c=np.dot(a,b)
-toc=time.time()
 
-print (c)
-print("Vectorized version :" + str((toc-tic)) + "ms")
+# Test numpy Array
+a = np.array([1,2,3,4])
+print(a)
+
+# Generate two vectors using numpy
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+tic = time.time() # Time in
+c = np.dot(a,b)
+toc = time.time() # Time out
+
+print(c)
+print("Vectorized version took:" + str((toc-tic)) + "ms")
 
 c=0
-tic=time.time()
+
+tic=time.time() # Time in
 for i in range (1000000):
-      c+= a[i]*b[i]
-toc=time.time()
-print (c)
-print ("for loop :" +str((toc-tic))+ "ms")
-      
-      
-      
-      
+      c += a[i]*b[i]
+toc=time.time() # Time out
+
+print(c)
+print("for loop took:" + str((toc-tic)) + "ms")
+
+# Inference: numpy's dot method takes lesser time than bruteforce looping
